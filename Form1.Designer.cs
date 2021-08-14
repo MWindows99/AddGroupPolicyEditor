a110e23d881537cfce0a0879f946194dbad72448
うぃーうぉん <80945906+MWindows99@users.noreply.github.com>
2021-08-14 15:00:15 +0900

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace AddGroupPolicyEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progress = new System.Windows.Forms.Label();
+            this.exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("游明朝 Demibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(647, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "作業を開始するにはボタンを押してください。";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(640, 75);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "作業を開始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(15, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "状況：";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // progress
+            // 
+            this.progress.AutoSize = true;
+            this.progress.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.progress.Location = new System.Drawing.Point(83, 128);
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(167, 20);
+            this.progress.TabIndex = 3;
+            this.progress.Text = "待機中 (準備完了)";
+            this.progress.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(507, 145);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(152, 50);
+            this.exit.TabIndex = 4;
+            this.exit.Text = "Exit";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 235);
+            this.ControlBox = false;
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.progress);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add GroupPolicyEditor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label progress;
+        private System.Windows.Forms.Button exit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddGroupPolicyEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Label文字変更
+            progress.Text = "Waiting...";
+
+            // 確認する
+            DialogResult dialogResult = MessageBox.Show(
+                "インストールを開始してもよろしいですか？",
+                "Question",
+                 MessageBoxButtons.YesNo,
+                 MessageBoxIcon.Information,
+                 MessageBoxDefaultButton.Button1);
+            if (dialogResult == DialogResult.Yes)
+            {
+                // ボタンを無効にする
+                button1.Enabled = false;
+                exit.Enabled = false;
+
+                // ここから処理
+
+                // プロセス起動情報構築
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+
+                // バッチファイル
+                startInfo.FileName = "cmd.exe";
+
+                // コマンド処理実行後、コマンドプロンプトを終了
+                startInfo.Arguments = "/c ";
+
+                // コマンド処理であるバッチファイル
+                startInfo.Arguments += @"instpro.bat ";
+
+                startInfo.CreateNoWindow = true; // コマンドプロンプトを非表示
+                startInfo.UseShellExecute = false; // シェル機能オフ
+
+                // バッチファイルを起動
+                var proc = Process.Start(startInfo);
+
+                // Label文字変更
+                progress.Text = "実行中...";
+
+                // 上記バッチ処理が終了するまで待機
+                proc.WaitForExit();
+
+                // Label文字変更
+                progress.Text = "終了.";
+
+                // メッセージを表示する
+                MessageBox.Show("インストールが完了しました。", "Info", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+
+                // 終了処理
+                Application.Exit();
+
+            }
+            else
+            {
+
+                // Label文字変更
+                progress.Text = "キャンセル.";
+
+                // メッセージを表示する
+                MessageBox.Show("キャンセルしました。","Info", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+
+                // 再実行に備える
+                progress.Text = "待機中 (準備完了)";
+            }
+
+
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
